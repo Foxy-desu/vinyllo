31324ea633e00ff7cfe973c27371a8823f936d5c
Alexandra Orliuk <iammeduza@gmail.com>
2024-05-02 15:00:06 +0300

    feat: add initial files

---
+++ vinyllo/src/App.tsx
@@ -0,0 +1,11 @@
+import { FormContainer } from './entities/auth_form/model/auth_form.container';
+
+function App() {
+  return (
+    <>
+      <FormContainer />
+    </>
+  );
+}
+
+export default App;
