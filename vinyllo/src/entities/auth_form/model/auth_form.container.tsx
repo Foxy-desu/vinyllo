3276c267e24575d2cdc60d7bab9f598e6a00260b
Alexandra Orliuk <iammeduza@gmail.com>
2024-05-02 14:54:52 +0300

    feat: implement form container component

--- vinyllo/src/entities/auth_form/model/auth_form.container.tsx
+++ vinyllo/src/entities/auth_form/model/auth_form.container.tsx
@@ -1,22 +1,47 @@
-import React from "react";
-import { Form } from "../ui/auth_form";
+import React from 'react';
+import { Form } from '../ui/auth_form';
+import useForm from '../utils/hooks/use_form';
 
 // TODO: implement authentification method for signIn state change
 // TODO: implement registration method for signUp state change
 // TODO: as soon as the user is created or found in the DB, redirect them to the main page.
 
 export const FormContainer = () => {
-    const [signInData, setSignInData] = React.useState({});
-    const [signUpData, setSignUpData] = React.useState({});
+  const [signInData, setSignInData] = React.useState({});
+  const [signUpData, setSignUpData] = React.useState({});
+  const { handleChange, handleSubmit, values, errors, submitError } = useForm(
+    setSignInData,
+    setSignUpData,
+  );
+  const [stopClick, setStopClick] = React.useState(false);
 
-    React.useEffect(()=> {
-        Object.keys(signUpData).length > 0 && console.log('sign up:' + JSON.stringify(signUpData));
-    },[signUpData])
+  React.useEffect(() => {
+    if (Object.keys(errors).length === 0 && Object.keys(values).length === 2) {
+      setStopClick(false);
+    } else {
+      setStopClick(true);
+    }
+  }, [errors]);
 
-    React.useEffect(()=> {
-        Object.keys(signInData).length > 0 && console.log('sign in:' + JSON.stringify(signInData));
-    },[signInData])
+  React.useEffect(() => {
+    Object.keys(signUpData).length > 0 &&
+      console.log('sign up:' + JSON.stringify(signUpData));
+  }, [signUpData]);
 
-    return <Form setSignInData={setSignInData} setSignUpData={setSignUpData}/>
-     
-}
+  React.useEffect(() => {
+    Object.keys(signInData).length > 0 &&
+      console.log('sign in:' + JSON.stringify(signInData));
+  }, [signInData]);
+
+  return (
+    <Form
+      setSignInData={setSignInData}
+      setSignUpData={setSignUpData}
+      values={values}
+      errors={errors}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+      stopClick={stopClick}
+    />
+  );
+};
