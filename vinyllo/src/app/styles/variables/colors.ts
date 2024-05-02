e2ecdafea0f9958b15e18ecdc849a0103279f646
Alexandra Orliuk <iammeduza@gmail.com>
2024-05-02 14:50:51 +0300

    feat: add color variables for the project

---
+++ vinyllo/src/app/styles/variables/colors.ts
@@ -0,0 +1,18 @@
+const myColors = {
+  dark: {
+    primary: '#393A5A',
+    primaryLt: '#706F8E',
+    secondary: '#22222E',
+    secondaryLt: '#ADA9BA',
+    secondaryLtR: '#E9E9E9',
+  },
+  light: {
+    primary: '#E1D5D9',
+    primaryLt: '#F6F2F6',
+    secondary: '#B7A7AE',
+    secondaryDrk: '#58545B',
+    secondaryDrkr: '#1C252C',
+  },
+};
+
+export default myColors;
