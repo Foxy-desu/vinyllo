28bef8526c106ede4aff504a06d25ec7915745be
Alexandra Orliuk <iammeduza@gmail.com>
2024-05-15 20:32:27 +0300

    feat: create reexports for pages

---
+++ vinyllo/src/pages/api/index.ts
@@ -0,0 +1,4 @@
+export {default as MainPageContainer} from "../main_page/model/mainPageContainer";
+export {default as MainPage} from "../main_page/ui/mainPage";
+export {default as LoginPage} from "../login_page/loginPage";
+export {default as Page404 } from "../404/404";
