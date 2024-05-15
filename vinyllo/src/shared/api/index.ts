dcb9adaa54e741728cb79fd83f2bd38e32bf29d1
Alexandra Orliuk <iammeduza@gmail.com>
2024-05-15 20:23:28 +0300

    feat: create file with reexports of shared components and types

---
+++ vinyllo/src/shared/api/index.ts
@@ -0,0 +1,10 @@
+//components
+export { default as AuthButton } from './../button/button';
+export { default as AuthInput } from "../input/input";
+export { default as VisibilityCheckBox } from './../visibility_checkbox/visibility_checkbox';
+export {default as ProtectedRoute} from "../protected_route/protectedRoute";
+
+//types
+export type {TAuthBtnProps, TAuthButton} from "../button/button";
+export type {TVCB, TVCBProps} from "../visibility_checkbox/visibility_checkbox";
+export type {TInputProps} from "../input/input";
