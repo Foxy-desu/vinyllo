da97d62311069900114229d8c9af5d59845c36f2
Alexandra Orliuk <iammeduza@gmail.com>
2024-05-16 18:57:41 +0300

    feat: add type exports

--- vinyllo/src/app/store/index.ts
+++ vinyllo/src/app/store/index.ts
@@ -10,3 +10,6 @@ const store = configureStore({
 });
 
 export default store;
+export type AppStore = typeof store;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch']
